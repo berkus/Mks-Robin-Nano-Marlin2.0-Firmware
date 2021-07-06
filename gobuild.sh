3fe50f23991c2e3021dbef20035760d3f7ebf855
Berkus Decker <berkus+github@metta.systems>
2021-07-06 23:38:16 +0300

    Add shebangs to scripts

--- gobuild.sh
+++ gobuild.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 platformio platform update
 platformio run -v -e mks_robin_nano_v3
 [ -d /Volumes/3DPRINT ] && cp .pio/build/mks_robin_nano_v3/Robin_nano_v3.bin /Volumes/3DPRINT/
--- gogdb.sh
+++ gogdb.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 ~/.platformio/packages/toolchain-gccarmnoneeabi/bin/arm-none-eabi-gdb .pio/build/mks_robin_nano_v3/firmware.elf
