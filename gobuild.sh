2480effeafae7d6c7b0a2ab1d695b96419648bb4
Berkus Decker <berkus+github@metta.systems>
2021-07-06 23:36:18 +0300

    Copy to sdcard only when it's mounted

--- gobuild.sh
+++ gobuild.sh
@@ -1,3 +1,4 @@
 platformio platform update
 platformio run -v -e mks_robin_nano_v3
-cp .pio/build/mks_robin_nano_v3/Robin_nano_v3.bin /Volumes/3DPRINT/
+[ -d /Volumes/3DPRINT ] && cp .pio/build/mks_robin_nano_v3/Robin_nano_v3.bin /Volumes/3DPRINT/
+
---
+++ gogdb.sh
@@ -0,0 +1,1 @@
+~/.platformio/packages/toolchain-gccarmnoneeabi/bin/arm-none-eabi-gdb .pio/build/mks_robin_nano_v3/firmware.elf
