fac9349255593ad2bc745bf06ee8d2170423031b
Berkus Decker <berkus+github@metta.systems>
2022-05-22 21:09:00 +0300

    Mark pins - some are erroneously marked?

--- Marlin/src/pins/stm32f4/pins_MKS_ROBIN_NANO_V3.h
+++ Marlin/src/pins/stm32f4/pins_MKS_ROBIN_NANO_V3.h
@@ -32,6 +32,57 @@
 
 #define BOARD_INFO_NAME "MKS Robin Nano V3"
 
+// Use soft PWM for fans - PWM is not working properly when paired with STM32 Arduino Core v1.7.0
+// This can be removed when Core version is updated and PWM behaviour is fixed.
+#define FAN_SOFT_PWM
+
+// USB Flash Drive support
+#define HAS_OTG_USB_HOST_SUPPORT
+
+// Avoid conflict with TIMER_TONE
+#define STEP_TIMER                            10
+
+// Use one of these or SDCard-based Emulation will be used
+//#define SRAM_EEPROM_EMULATION                   // Use BackSRAM-based EEPROM emulation
+//#define FLASH_EEPROM_EMULATION                  // Use Flash-based EEPROM emulation
+#define I2C_EEPROM
+#define MARLIN_EEPROM_SIZE                0x1000  // 4KB
+
+#define I2C_SCL_PIN                       PB6
+#define I2C_SDA_PIN                       PB7
+
+//
+// Release PB4 (Z_DIR_PIN) from JTAG NRST role
+//
+//#define DISABLE_DEBUG
+
+//
+// Servos
+//
+#define SERVO0_PIN                          PA8   // Enable BLTOUCH -- it's not a servo, but a Hall sensor, wtf?
+
+//
+// Limit Switches
+//
+#define X_DIAG_PIN                          PD15 // E1_STEP     -- error?
+#define Y_DIAG_PIN                          PD2  // Y- endstop
+#define Z_DIAG_PIN                          PC8  // Z- endstop
+#define E0_DIAG_PIN                         PC4  // Z+ endstop
+#define E1_DIAG_PIN                         PE7  // E1- endstop
+
+//
+#define X_STOP_PIN                          PA15
+#define Y_STOP_PIN                          PD2
+#define Z_MIN_PIN                           PC8
+#define Z_MAX_PIN                           PC4
+
+//
+// Steppers
+//
+#define X_ENABLE_PIN                        PE4
+#define X_STEP_PIN                          PE3
+#define X_DIR_PIN                           PE2
+
 #ifndef X_CS_PIN
   #define X_CS_PIN                          PD5
 #endif
